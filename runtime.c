107be75b9816a62ab0757082cc07b72eb768b0f2
Yuanbo Fan <yfw492@nightwing.ece.northwestern.edu>
2012-10-24 13:28:49 -0500

    yuanbo

--- runtime.c
+++ runtime.c
@@ -70,18 +70,20 @@
 /************Global Variables*********************************************/
 
 #define NBUILTINCOMMANDS (sizeof BuiltInCommands / sizeof(char*))
-
+/*
 typedef struct bgjob_l
 {
   pid_t pid;
   struct bgjob_l* next;
 } bgjobL;
-
+*/
+int jobNum = 1;
 /* the pids of the background processes */
 bgjobL *bgjobs = NULL;
 /* the pid of fore ground process, if the shell is in fore ground */ 
 pid_t fgjob = 0;
-
+status_t fgStatus;
+char* fgCmd;
 /************Function Prototypes******************************************/
 /* run command */
 static void
@@ -122,7 +124,10 @@ findCommand(commandT* cmd);
 void
 RunCmd(commandT* cmd)
 {
-  RunCmdFork(cmd, TRUE);
+	if (strcmp(cmd->argv[cmd->argc-1], "&") == 0)
+		RunCmdFork(cmd, FALSE);
+	else
+  	RunCmdFork(cmd, TRUE);
 } /* RunCmd */
 
 
@@ -362,28 +367,59 @@ ResolveExternalCmd(commandT* cmd)
 static void
 Exec(commandT* cmd, bool forceFork)
 {
-  if (forceFork) 
-    {
-      pid_t child_id = fork();
-      if (child_id != 0) 
-	{
-	  /* parent should wait */
-	  fgjob = child_id;
-	  waitpid(child_id, NULL, 0);
-	  fgjob = 0;
-	}
-      else 
-	{
-	  char * fullPath = findCommand(cmd);
-	  execv(fullPath, cmd->argv);
-	  free(fullPath);
-	}
-    }
-  else
-    {
-    }
-} /* Exec */
+	sigset_t chldSigset;
+	sigemptyset(&chldSigset);
+	sigaddset(&chldSigset, SIGCHLD);
+	sigprocmask(SIG_BLOCK, &chldSigset, NULL);
 
+	pid_t child_id = fork();
+	if (child_id == 0) // child
+	{
+		char* fullPath = findCommand(cmd);
+		setpgid(0, 0);
+		sigprocmask(SIG_UNBLOCK, &chldSigset, NULL);
+		if (!forceFork)
+			cmd->argv[cmd->argc-1] = 0;
+//		{
+//			free(cmd->argv[cmd->argc-1]);
+//			cmd->argc--;
+//		}
+//		printf("%s\n", cmd->argv[cmd->argc-1]);
+		execv(fullPath, cmd->argv);
+	}
+	else // parent
+	{
+		char* command = malloc(PATHBUFSIZE);
+		strcpy(command, cmd->argv[0]);
+		int i;
+		for (i = 1; i < cmd->argc; i++)
+		{
+			strcat(command, " ");
+			strcat(command, cmd->argv[i]);
+		}
+
+		if (forceFork)
+		{
+			fgjob = child_id;
+			fgStatus = BUSY;
+			strcpy(fgCmd, command);
+
+			sigprocmask(SIG_UNBLOCK, &chldSigset, NULL);
+			while (fgStatus != AVAIL)
+			{
+				sleep(1);
+			}
+		}
+		else
+		{	
+			fgjob = 0;
+			fgStatus = AVAIL;
+			addJob(command, child_id, RUNNING);
+			sigprocmask(SIG_UNBLOCK, &chldSigset, NULL);
+		}
+		free(command);
+	}
+} /* Exec */
 
 /*
  * IsBuiltIn
@@ -402,7 +438,10 @@ IsBuiltIn(char* cmd)
 {
   if (strcmp(cmd, "cd") == 0 ||  
       strcmp(cmd, "exit") == 0 ||
-      strchr(cmd, '=') != NULL    /* set env */
+      strcmp(cmd, "bg") == 0 ||
+      strcmp(cmd, "fg") == 0 ||
+      strcmp(cmd, "jobs") == 0 ||
+      strchr(cmd, '=') != NULL   /* set env */
       )
     {
       return TRUE;
@@ -427,6 +466,8 @@ IsBuiltIn(char* cmd)
 static void
 RunBuiltInCmd(commandT* cmd)
 {
+	bgjobL* job;
+	int num;
   if (strcmp(cmd->name, "cd") == 0) 
     {
       char * newPath;
@@ -479,6 +520,78 @@ RunBuiltInCmd(commandT* cmd)
       free(var);
       fflush(stdout);
     }
+	else if (strcmp(cmd->name, "bg") == 0)
+	{
+		if (cmd->argc == 2)
+		{
+			num = atoi(cmd->argv[1]);
+			job = searchJobByNum(num);
+			
+			if (job != NULL && job->state == STOPPED)
+				kill(-job->pid, SIGCONT);
+		}
+		else
+		{
+			job = searchJobByState(STOPPED);
+			if (job != NULL)
+				kill(-job->pid, SIGCONT);
+		}
+	}
+	else if (strcmp(cmd->name, "fg") == 0)
+	{
+		if (cmd->argc == 2)
+		{
+			num = atoi(cmd->argv[1]);
+			job = searchJobByNum(num);
+			
+			if(job != NULL && job->state == RUNNING)
+			{
+				fgjob = job->pid;
+				fgStatus = BUSY;
+				strcpy(fgCmd, job->cmd);
+				removeJob(job);
+				while (fgStatus != AVAIL)
+				{
+					sleep(1);
+				}
+			}
+		}
+		else
+		{
+			job = searchJobByState(RUNNING);
+			if (job != NULL)
+			{
+				fgjob = job->pid;
+				fgStatus = BUSY;
+				strcpy(fgCmd, job->cmd);
+				removeJob(job);
+				while (fgStatus != AVAIL)
+				{
+					sleep(1);
+				}
+			}
+		}
+	}
+	else if (strcmp(cmd->name, "jobs") == 0)
+	{
+		int i;
+		bgjobL* job;
+		for (i = 1; i < jobNum; i++)
+		{
+			job = searchJobByNum(i);
+			if (job != NULL && job->state == RUNNING)
+			{
+				printf("[%d]\tRunning\t\t\t%s\n", job->num, job->cmd);
+				fflush(stdout);
+			}
+			if (job != NULL && job->state == STOPPED)
+			{
+				printf("[%d]\tStopped\t\t\t%s\n", job->num, job->cmd);
+				fflush(stdout);
+			}
+
+		}
+	}
 } /* RunBuiltInCmd */
 
 
@@ -494,6 +607,23 @@ RunBuiltInCmd(commandT* cmd)
 void
 CheckJobs()
 {
+/*
+	bgjobL* job = bgjobs;
+	bgjobL* temp = NULL;
+	while (job != NULL)
+	{
+		if (job->state == TERMINATED)
+		{
+//				printf("[%d]\tDone\t\t\t%s\n", job->num, job->cmd);
+				fflush(stdout);
+				temp = job;
+				job = job->next;
+				removeJob(temp);
+		}
+		else
+			job = job->next;
+	}
+*/
 } /* CheckJobs */
 
 /*
@@ -558,4 +688,117 @@ PrintPrompt()
   free(promptout);
   fflush(stdout); /* make sure the prompt is printed */
   return;
+}
+
+void
+transitProcState(bgjobL* job, state_t newState)
+{
+	bgjobL* temp = NULL;
+
+	job->state = newState;
+	if ((newState != TERMINATED) && (job->prev != NULL))
+	{
+		temp = job->prev;
+		temp->next = job->next;
+		temp = job->next;
+		temp->prev = job->prev;
+		job->prev = NULL;
+		job->next = bgjobs;
+		bgjobs = job;
+	}
+}
+
+bgjobL*
+searchJobByID(pid_t pid)
+{
+	bgjobL* job = bgjobs;
+
+	while (job != NULL)
+	{
+		if (job->pid == pid)
+			return job;
+		job = job->next;
+	}
+	return job;
+}
+
+bgjobL*
+searchJobByNum(int num)
+{
+	bgjobL* job = bgjobs;
+	while (job != NULL)
+	{
+		if (job->num == num)
+			return job;
+		job = job->next;
+	}
+	return job;
+}
+
+bgjobL*
+searchJobByState(state_t state)
+{
+	bgjobL* job = bgjobs;
+	while (job != NULL)
+	{
+		if (job->state == state)
+			return job;
+		job = job->next;
+	}
+	return job;
+}
+
+void
+addJob(char* command, pid_t pid, state_t state)
+{
+		bgjobL* newProc = malloc(sizeof(bgjobL));
+		newProc->cmd = malloc(PATHBUFSIZE);
+		newProc->pid = pid;
+		newProc->state = state;
+		newProc->num = jobNum++;
+		strcpy(newProc->cmd, command);	
+		newProc->prev = NULL;
+		newProc->next = bgjobs;
+		bgjobs = newProc;
+			
+#ifdef DEBUG_OUTPUT
+			printf("jobs in background!\n");
+			while (newProc != NULL)
+			{
+				printf("pid: %d\nstate: %d\n", newProc->pid, newProc->state);
+				newProc = newProc->next;
+			}
+#endif
+}
+
+void
+removeJob(bgjobL* job)
+{
+	bgjobL* temp;
+	if (job->prev == NULL && job->next == NULL) // ONLY JOB
+	{
+		bgjobs = NULL;
+	}
+	else if (job->prev == NULL) // FIRST JOB
+	{
+		temp = job->next;
+		temp->prev = NULL;
+		bgjobs = temp;
+	}
+	else if (job->next == NULL) // LAST JOB
+	{
+		temp = job->prev;
+		temp->next = NULL;
+	}
+	else
+	{
+		temp = job->prev;
+		temp->next = job->next;
+		temp = job->next;
+		temp->prev = job->prev;
+	}
+	job->prev = NULL;
+	job->next = NULL;
+	free(job->cmd);
+	free(job);
 }
